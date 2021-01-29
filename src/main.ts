9c5e0db2bda1e2ad50ea66c197284dd237d5f1a6
Raskolnikov <jerry.tann@opteyes.cn>
2021-01-29 15:06:23 +0800

    feat: add swagger/openapi

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // for global middleware - app.use(logger) 
   // global scoped filters - app.useGlobalFilters(new HttpExceptionFilter());
   // for global guard - app.useGlobalGuards(new RolesGuard());
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
